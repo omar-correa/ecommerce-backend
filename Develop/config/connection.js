e2c5174e79870200408affa48151acb45f9711c9
Omar Correa <omarcorrea378@yahoo.com>
2023-01-18 17:10:42 -0500

    completed code for the routes folders

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -6,7 +6,10 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
+      user: "root",
+      password: "root",
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
